eba7069cb63681be94adfcf6406ef88ed637ad1b
vishnuchaphe <vishnuchaphe0@gmail.com>
2026-06-29 21:25:41 +0530

    Add solutions for q002 and q003

---
+++ phases/phase_1_while_loop/q002_print_10_to_1.py
@@ -0,0 +1,13 @@
+"""
+Question 002: Print Numbers 10 to 1 (Reverse)
+Difficulty: Beginner | Time: 5 minutes
+"""
+
+def print_reverse():
+    i = 10
+    while i >= 1:
+        print(i)
+        i -= 1
+
+if __name__ == "__main__":
+    print_reverse()
---
+++ phases/phase_1_while_loop/q003_even_numbers.py
@@ -0,0 +1,13 @@
+"""
+Question 003: Print Even Numbers (1 to 100)
+Difficulty: Beginner | Time: 8 minutes
+"""
+
+def print_even():
+    i = 2
+    while i <= 100:
+        print(i)
+        i += 2
+
+if __name__ == "__main__":
+    print_even()
